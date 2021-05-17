de85816f7396fc4ed1fa1fb760598da566616efa
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-05-17 11:27:38 +0530

    Update forms section

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,69 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [details, setDetails] = useState([]);
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const addToList = (e) => {
+    e.preventDefault();
+    if (name && email) {
+      setDetails((prevValue) => [
+        ...prevValue,
+        { id: new Date().getTime().toString(), name: name, email: email },
+      ]);
+    }
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <article>
+          <form className="form">
+            <div className="form-control">
+              <label htmlFor="firstName">Name :</label>
+              <input
+                id="firstName"
+                type="text"
+                name="firstName"
+                value={name}
+                onChange={onNameChange}
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="email">Email :</label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={email}
+                onChange={onEmailChange}
+              />
+            </div>
+            <button onClick={addToList}>Add Person</button>
+          </form>
+          {details.map((detail) => {
+            return (
+              <div className="item" key={detail.id}>
+                <h4>{detail.name}</h4>
+                <p>{detail.email}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default ControlledInputs;
--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -7,61 +7,75 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [people, setPeople] = useState([]);
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [age, setAge] = useState("");
+  const [person, setPerson] = useState({ name: "", email: "", age: "" });
+  const [details, setDetails] = useState([]);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
-    } else {
-      console.log('empty values');
+    if (person.name && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setDetails((prevValue) => [...prevValue, newPerson]);
     }
+    setPerson({ name: "", email: "", age: "" });
   };
+
   return (
-    <>
+    <React.Fragment>
       <article>
-        <form className='form' onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <label htmlFor='firstName'>Name : </label>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="name">Name : </label>
             <input
-              type='text'
-              id='firstName'
-              name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              type="text"
+              id="name"
+              name="name"
+              value={person.name}
+              onChange={handleChange}
             />
           </div>
-          <div className='form-control'>
-            <label htmlFor='email'>Email : </label>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
             <input
-              type='email'
-              id='email'
-              name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              name="email"
+              value={person.email}
+              onChange={handleChange}
             />
           </div>
-          <button type='submit'>add person</button>
+          <div className="form-control">
+            <label htmlFor="age">Age : </label>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              value={person.age}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">add person</button>
         </form>
-        {people.map((person, index) => {
-          const { id, firstName, email } = person;
+        {details.map((detail) => {
+          const { name, email, age, id } = detail;
           return (
-            <div className='item' key={id}>
-              <h4>{firstName}</h4>
+            <div className="item" key={id}>
+              <h4>{name}</h4>
               <p>{email}</p>
+              <p>{age}</p>
             </div>
           );
         })}
       </article>
-    </>
+    </React.Fragment>
   );
 };
 
