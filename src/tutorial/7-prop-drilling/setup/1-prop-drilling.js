0eb0b57421d47c3a252b1a915bff508f21d033eb
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-05-23 09:27:33 +0530

    Update prop-drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
+// const getLocalData = (data) => {
+//   if (data) {
+//     return JSON.parse(localStorage.getItem("people"));
+//   }
+// };
+
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  // const [people, setPeople] = useState(getLocalData(data));
+  const [people, setPeople] = useState(data);
+
+  localStorage.setItem("people", JSON.stringify(people));
+
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <h3>Prop Drilling</h3>
+        <List people={people} removePerson={removePerson} />
+      </section>
+    </React.Fragment>
+  );
+};
+
+const List = ({ people, removePerson }) => {
+  return (
+    <React.Fragment>
+      {people.map((person, i) => {
+        return (
+          <SinglePerson key={i} person={person} removePerson={removePerson} />
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+const SinglePerson = ({ person, removePerson }) => {
+  return (
+    <React.Fragment>
+      <div className="item">
+        <h4>{person.name}</h4>
+        <button onClick={() => removePerson(person.id)}>Remove</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default PropDrilling;
