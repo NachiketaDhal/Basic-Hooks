252d19a7edf20475f6ee070dccee8f1b029e2b55
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-06-02 09:51:21 +0530

    Update custom hooks

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const fetchedProducts = await fetch(url);
+    const fetchedProductsJSON = await fetchedProducts.json();
+    setProducts(fetchedProductsJSON);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { loading, products };
+};
